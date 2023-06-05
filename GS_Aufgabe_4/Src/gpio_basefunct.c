578a6c57b81b349ba2040c0dfbe9cdfd5ca81e92
rumagr <rumaringr@gmail.com>
2023-06-05 11:19:30 +0200

    Aufgabe 4 Magic numbers aus gpio_basefunct entfernt

--- GS_Aufgabe_4/Src/gpio_basefunct.c
+++ GS_Aufgabe_4/Src/gpio_basefunct.c
@@ -3,49 +3,55 @@
 #include "lcd.h"
 
 #define PIN1 1 
-#define Pin0 0  
+#define PIN0 0  
+#define BIT_SET 1 
+#define BIT_NOT_SET 0
+#define BIT_MASK 0x01
+#define BSSR_HIGH 16
+#define MODER_MASK_IN (0x03 << (2*0))
+#define MODER_MASK_OUT 0x01 <<(2*0)
 
 void init_GPIO(void)
 {
-	GPIOD->OTYPER &= ~(1 << PIN1); 
-	GPIOD->BSRR = (1 << PIN1); 
+	GPIOD->OTYPER &= ~(BIT_SET << PIN1); 
+	GPIOD->BSRR = (BIT_SET << PIN1); 
 	open_drain(); 
 }
 
 void open_drain(void)
 {
-	GPIOD->OTYPER |= (1<<0);
+	GPIOD->OTYPER |= (BIT_SET << PIN0);
 }
 
 void push_pull(void)
 {
-	GPIOD->OTYPER &= ~(1 << 0); 
+	GPIOD->OTYPER &= ~(BIT_SET << PIN0); 
 }
 
 void GPIO_In(void)
 {
-	GPIOD->MODER &= ~(0x03 << (2*0)); 
+	GPIOD->MODER &= ~(MODER_MASK_IN); 
 }
 
 void GPIO_Out(void)
 {
-	GPIOD->MODER |= (0x01 <<(2*0)); 
+	GPIOD->MODER |= (MODER_MASK_OUT); 
 }
 
 void GPIO_High(void)
 {
-	GPIOD->BSRR = (1 << 0); 
+	GPIOD->BSRR = (BIT_SET << PIN0); 
 }
 
 void GPIO_Low(void)
 {
-	GPIOD->BSRR = ((1 << 0) << 16); 
+	GPIOD->BSRR = ((BIT_SET << PIN0) << BSSR_HIGH); 
 }
 
 
 void GPIO_Read(uint8_t *var, uint32_t shift)
 {
-	uint8_t temp = (GPIOD->IDR & 0x01); 
+	uint8_t temp = (GPIOD->IDR & BIT_MASK); 
 
 	//if(temp)   //test
 	//{
